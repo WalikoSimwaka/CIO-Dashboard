e563542e5fd19a05d5d02fba4c1d2eacda26505b
walikosimwaka <waliko.simwaka@nbs.mw>
2025-06-25 06:08:57 +0200

    final4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
-dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
