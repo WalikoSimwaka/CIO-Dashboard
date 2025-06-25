4ded8119d21499daec156e3a343c048f860d6173
walikosimwaka <waliko.simwaka@nbs.mw>
2025-06-25 06:11:04 +0200

    final5

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
-// All routes serve the index.html
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "index.html"));
 });
