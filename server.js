e95628019e4d5cbf6f6275631ed27192f3efba49
walikosimwaka <waliko.simwaka@nbs.mw>
2025-06-25 06:04:45 +0200

    final2

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// All routes serve the index.html
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+});
